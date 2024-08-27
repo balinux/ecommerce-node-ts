17be439c0d9b35377204f6ce7286867758c908e4
rio <rio@news-MacBook-Air.local>
2024-08-27 11:15:24 +0800

    Temui kreator Satu Hari Satu FotoTemui kreator Satu Hari Satu Foto4:35 / 5:21Route definitions

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express, { Express, Request, Response } from "express";
 import { PORT } from "./secrets";
+import rootRouter from "../routes";
 
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("work fine");
-});
+app.use("/api", rootRouter);
 
 app.listen(PORT, () => {
   console.log("app run on port 3000");
